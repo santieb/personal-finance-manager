8073ad47ab6dcebcad5c05a67bfaa6f52c3916d1
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-10-13 11:11:35 -0300

    change: jwt payload

--- backend/src/auth/utils/jwt.js
+++ backend/src/auth/utils/jwt.js
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken'
 import { config } from '../../config/index.js'
 const { secretKey } = config
 
-const generateToken = (payload) => jwt.sign({ data: payload }, secretKey, { expiresIn: 60 * 60 })
+const generateToken = (payload) => jwt.sign({ id: payload }, secretKey, { expiresIn: 60 * 60 })
 
 export { generateToken }
