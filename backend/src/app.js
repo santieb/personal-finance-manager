def7237a8e86852bfdcc5ecfe3ce6f3d160d4fef
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2022-10-18 13:14:02 -0300

    healthCheck

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import routes from './shared/router.js'
+import healthCheckRoutes from './config/healthCheck.js'
 
 const { port } = config
 const app = express()
@@ -12,6 +13,8 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
+
+app.use('/healthcheck', healthCheckRoutes)
 app.use('/api', routes)
 
 app.listen(port, () => {
